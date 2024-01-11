cc01bd7bb5777b2d877de088bc15d4c5e82decdf
jadsongmatos <jadson.g-matos@outlook.com>
2024-01-11 02:21:17 -0300

    copy_nonoverlapping

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,58 @@
-use linuxfb;
-use std::{thread, time};
+use linuxfb::Framebuffer;
+use std::{ptr, thread, time};
 
-fn main(){
-    let devices = linuxfb::Framebuffer::list().unwrap();
+fn main() {
+    let devices = Framebuffer::list().unwrap();
     println!("Devices: {:?}", devices);
 
-    // Instead of hardcoding the path, you could also use `Framebuffer::list()`
-    // to find paths to available devices.
-    let fb = linuxfb::Framebuffer::new("/dev/fb0").unwrap();
-
+    let fb = Framebuffer::new("/dev/fb0").unwrap();
     println!("Size in pixels: {:?}", fb.get_size());
-
     println!("Bytes per pixel: {:?}", fb.get_bytes_per_pixel());
-
     println!("Physical size in mm: {:?}", fb.get_physical_size());
 
-    // Map the framebuffer into memory, so we can write to it:
-    let mut data = fb.map().unwrap();
+    let frame_duration = time::Duration::from_millis(1000 / 3); // Duração de um quadro para 3 FPS
 
-    //1049088 1366x768
-    //4196352 1366x768x4
-    println!("data: {:?}",data.len());
+    let mut display_buffer = fb.map().unwrap();
+    let mut offscreen_buffer = vec![0; display_buffer.len()];
 
+    let red: u8 = 0xBF;
+    let green: u8 = 0xAF;
+    let blue: u8 = 0xCF;
 
-    let frame_duration = time::Duration::from_millis(33); // Duração de um quadro para 30 FPS
+    let mut cor = false;
 
-    // Define your color in RGB:
-    let red: u8 = 0xBF; // Adjust the intensity of red (0 to 255)
-    let green: u8 = 0xAF; // Adjust the intensity of green (0 to 255)
-    let blue: u8 = 0xCF; // Adjust the intensity of blue (0 to 255)
-
-    // Modify the loop to set this color:
     loop {
         let start = time::Instant::now(); // Marca o início do quadro
 
-        for pixel in data.chunks_exact_mut(4) {
-            pixel[0] = blue;
-            pixel[1] = green;
-            pixel[2] = red;
+        // Draw to the off-screen buffer
+        for pixel in offscreen_buffer.chunks_exact_mut(4) {
+            if cor {
+                pixel[0] = blue;
+                pixel[1] = green;
+                pixel[2] = red;
+            } else {
+                pixel[0] = 0xFF;
+                pixel[1] = 0xFF;
+                pixel[2] = 0xFF;
+            }
         }
-      
-        let elapsed = start.elapsed(); // Calcula o tempo gasto na renderização
 
+        cor = !cor;
+
+        // Swap buffers (copy off-screen buffer to the display buffer)
+        //display_buffer.copy_from_slice(&offscreen_buffer);
+        unsafe {
+            ptr::copy_nonoverlapping(offscreen_buffer.as_ptr(), display_buffer.as_mut_ptr(), offscreen_buffer.len());
+        }
+
+        let elapsed = start.elapsed(); // Calcula o tempo gasto na renderização
         if elapsed < frame_duration {
             // Se a renderização foi mais rápida do que a duração de um quadro, pausa o loop
             thread::sleep(frame_duration - elapsed);
-            
+        } else {
+            println!("lento: {:?}",elapsed);
         }
-        println!("frame_duration: {:?}",elapsed)
+
+        println!("frame_duration: {:?}",elapsed);
     }
 }
